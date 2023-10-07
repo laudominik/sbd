1a0c51074a29de141055581cde79d76346085541
Dominik Lau <harek2388@gmail.com>
2023-10-07 16:49:01 +0200

    report, experiment table

--- test/perf1.cpp
+++ test/perf1.cpp
@@ -12,12 +12,14 @@ using namespace sbd::impl;
 inline void theoretical(uint64_t n){
     std::cout << "[Theoretical] i/o: "
     << 4 * n * static_cast<uint64_t>(ceil(log2(n)-1u)) * constants::RECORD_SIZE / constants::PAGE_SIZE
+    << " phases: " << ceil(log2(n)-1u)
     << std::endl;
 }
 
 TEST(Perf1, testCase1){
-    static constexpr auto records = {10, 100, 1000, 10000, 100000};
+    static constexpr auto records = {10, 100, 1000, 5000, 10000, 25000, 50000, 100000, 200000};
     std::srand(std::time(nullptr));
+    std::cout << "b = " << constants::PAGE_SIZE / constants::RECORD_SIZE << std::endl;
     for(const auto n : records){
         std::cout << "OoOoOoOoOoOoOo " << n << " RECORDS oOoOoOoOoOoOoO" << std::endl;
         theoretical(n);
